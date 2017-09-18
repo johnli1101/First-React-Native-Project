45f33cf0ba55f9fdd617c2f9893d8ca5e2ef0702
John Li <john.li.11011972@gmail.com>
2017-09-18 16:00:00 -0700

    Finished Text Input Tutorial

--- index.android.js
+++ index.android.js
@@ -9,12 +9,51 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  Image
 } from 'react-native';
 
 export default class new_project extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {text: ''};
+   }
+
   render() {
+    let pic = {
+      uri: 'https://previews.123rf.com/images/flybird163/flybird1631211/flybird163121100148/16611277-hello-world-title-written-with-chalk-on-blackboard-Stock-Photo.jpg'
+    };
+    let pic2 = {
+      uri: 'https://cdn.programiz.com/sites/tutorial2program/files/hello-world.jpg'
+    };
+    let pic3 = {
+      uri: 'https://i.ytimg.com/vi/zecueq-mo4M/maxresdefault.jpg'
+    };
     return (
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+      /*
+      <View style={{
+        flex: 1,
+        flexDirection:'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}>
+        <Image source={pic} style={{width: 100, height:100}}/>
+        <Image source={pic2} style={{width: 100, height:100}}/>
+        <Image source={pic3} style={{width: 100, height:100}}/>
+      </View>
+      */
+      /*
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
@@ -27,9 +66,21 @@ export default class new_project extends Component {
           Shake or press menu button for dev menu
         </Text>
       </View>
+      */
     );
   }
 }
+
+const myStyles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
